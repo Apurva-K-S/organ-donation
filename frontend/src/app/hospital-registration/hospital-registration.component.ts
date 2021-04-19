a5104e54f926588115d7684508cc0d3bcfbf9bb3
chandan <chandantejaa@gmail.com>
2021-04-19 14:17:16 +0530

    added hosp reg page

--- frontend/src/app/hospital-registration/hospital-registration.component.ts
+++ frontend/src/app/hospital-registration/hospital-registration.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface States{
+  value: string;
+  viewValue: string;
+}
+
+
 
 @Component({
   selector: 'app-hospital-registration',
@@ -7,9 +15,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospitalRegistrationComponent implements OnInit {
 
-  constructor() { }
+  hospForm: FormGroup = new FormGroup({});
+
+  states: States[]=[
+    {value: 'Andhra Pradesh', viewValue: 'Andhra Pradesh'},
+    {value: 'Telangana', viewValue: 'Telangana'},
+    {value: 'Maharashtra', viewValue: 'Maharashtra'},
+    {value: 'Tamilnadu', viewValue: 'Tamilnadu'},
+    {value: 'Karnataka', viewValue: 'Karnataka'},
+    {value: 'Kerala', viewValue: 'Kerala'},
+    {value: 'Delhi', viewValue: 'Delhi'},
+    {value: 'Orissa', viewValue: 'Orissa'},
+    {value: 'Gujarat', viewValue: 'Gujarat'},
+    {value: 'West Bengal', viewValue: 'West Bengal'},
+  
+    ];
+  
+
+  constructor(private fb: FormBuilder) { 
+
+    this.hospForm = fb.group({
+      
+      hospName: ['', [Validators.required]],
+      hospEmail: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      city:  ['', [Validators.required]],
+      pin:   ['', [Validators.required]],
+      
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  get f(){
+    return this.hospForm.controls;
+  }
+
+  submit()
+  {
+
+  }
+ 
 
 }
